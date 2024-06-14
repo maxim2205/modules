f77f1e04ae55ac1d12e6e19536c175bca739aaf8
Maxim <chertin.msk@gmail.com>
2024-06-14 09:33:50 +0300

    1

--- src/get_geo.js
+++ src/get_geo.js
@@ -1,6 +1,5 @@
 const get_geo = async () => {
     let response = await fetch("https://pro.ip-api.com/json/?fields=status,message,country,countryCode,countryCode3,region,regionName,city,zip,lat,lon,timezone,offset,callingCode,isp,org,as,mobile,proxy,hosting,query&key=vCJSQarNduxSd9M");
-
     if ( response.ok ) {
         let json = await response.json();
         if ( json.status !== 'success' ) {
